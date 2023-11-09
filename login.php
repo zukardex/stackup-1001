6df6d69c7123be0553023b53cd79cd6a7ca44088
alphinfrancis25 <alphinfrancis25@gmail.com>
2023-11-09 18:07:27 +0530

    test

--- login.php
+++ login.php
@@ -1,39 +1,47 @@
 <?php
-include 'config.php';
-$email=$_POST['email'];
-$pass=$_POST['password'];
+include 'credentials.php';
 
-function userExists($email){
-    $sql = "SELECT * FROM users WHERE email = '$email'";  
-    $result =mysqli_query($conn, $sql);  
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-          
-        if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";
+// Initialize the $conn variable
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-            return true;
-        }
-}
-function authenticate($email, $pass){
-    if(userExists($email)){
-        //DATABASE FETCHING AND CHECKING NEEDS TO BE ADDED HERE, return false here if email and password dont match
-        $sql= "SELECT * FROM users WHERE email='$email' AND password='$pass'"
-        $result=mysqli_query($conn, $sql);
-        $count =mysqli_num_rows($result);
-        if($count==1){
-            return true
-        }
-    }else{
-        echo "<h1><center> Password don't match </center></h1>";
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+function userExists($email, $conn) {
+    $sql = "SELECT * FROM users WHERE email = '" . $email . "';";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+        return true;
+    } else {
         return false;
     }
 }
 
+function authenticate($email, $pass, $conn) {
+    if (userExists($email, $conn)) {
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$pass';";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
 
-if(authenticate($email,$pass)){
-    //login successfull
-    echo '<script> document.location="home.html"; </script>';
+        if ($count == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
+
+if (authenticate($email, $pass, $conn)) {
+    // Login successful
+    echo '<script> document.location = "home.html"; </script>';
+}
+
+$conn->close();
 
 ?>
